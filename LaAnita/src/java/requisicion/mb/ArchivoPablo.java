a40b4c300a2288484783bfb751d3962c2983e3c9
Comodoro <Comodoro@pjgt>
2013-06-15 03:43:14 -0500

    metodoAgregar

--- LaAnita/src/java/requisicion/mb/ArchivoPablo.java
+++ LaAnita/src/java/requisicion/mb/ArchivoPablo.java
@@ -11,11 +11,12 @@ package requisicion.mb;
 public class ArchivoPablo {
     
     public void mensaje(){
-        
         System.out.println("hola");
-        
-        
+    }
     
+    
+    public void preparandoArchivo(){
+        
     }
     
 }
